edcec3c67f0e1043751ab802b21216eef6e4b78b
David Howe <112756177+davidhowe1@users.noreply.github.com>
2022-11-28 14:47:39 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,441 @@
+const propertyListings = [
+
+    {
+        images: 'https://media.rightmove.co.uk/75k/74061/127067960/74061_HSS248313_IMG_02_0000.jpeg',
+        address: 'Finch Lea Drive, Liverpool, L14',
+        description: 'A bright modern two bedroom apartment situated on the first floor of this popular development on Highfield Street which runs parallel with Pall Mall close to its junction with Leeds Street. The property is close to Liverpool John Moores University and immediate vacant possession is available upon completion.',
+        price: 120000,
+        bedrooms: 3,
+        bathrooms: 1,
+        type: 'Semi-Detached',
+        newHome: true,
+        recentlyAdded: false,
+    },
+
+    {   
+        images: 'https://media.rightmove.co.uk/120k/119425/126063677/119425_SEF220610_IMG_00_0000.jpeg',
+        address: 'Harris Drive, Liverpool, L20',
+        description: 'A bright modern two bedroom apartment situated on the first floor of this popular development on Highfield Street which runs parallel with Pall Mall close to its junction with Leeds Street. The property is close to Liverpool John Moores University and immediate vacant possession is available upon completion.',
+        price: 160000,
+        bedrooms: 3,
+        bathrooms: 1,
+        type: 'Semi-Detached',
+        newHome: false,
+        recentlyAdded: true,
+    },
+
+    {
+        images: 'https://media.rightmove.co.uk/204k/203528/127002389/203528_1433138-1_IMG_00_0000.jpeg',
+        address: 'Carnarvon Road, Liverpool, L9',
+        description: 'A bright modern two bedroom apartment situated on the first floor of this popular development on Highfield Street which runs parallel with Pall Mall close to its junction with Leeds Street. The property is close to Liverpool John Moores University and immediate vacant possession is available upon completion.',
+        price: 89950,
+        bedrooms: 3,
+        bathrooms: 1,
+        type: 'Terraced',
+        newHome: false,
+        recentlyAdded: true,
+    },
+
+    {
+        images: 'https://media.rightmove.co.uk/73k/72919/129159044/72919_18276_IMG_00_0000.jpeg',
+        address: '41 Butterfield Street, Liverpool',
+        description: 'A bright modern two bedroom apartment situated on the first floor of this popular development on Highfield Street which runs parallel with Pall Mall close to its junction with Leeds Street. The property is close to Liverpool John Moores University and immediate vacant possession is available upon completion.',
+        price: 60000,
+        bedrooms: 2,
+        bathrooms: 1,
+        type: 'Terraced',
+        newHome: false,
+        recentlyAdded: true,
+    },
+
+    {
+        images: 'https://media.rightmove.co.uk/75k/74061/127067960/74061_HSS248313_IMG_02_0000.jpeg',
+        address: 'Finch Lea Drive, Liverpool, L14',
+        description: 'A bright modern two bedroom apartment situated on the first floor of this popular development on Highfield Street which runs parallel with Pall Mall close to its junction with Leeds Street. The property is close to Liverpool John Moores University and immediate vacant possession is available upon completion.',
+        price: 120000,
+        bedrooms: 3,
+        bathrooms: 1,
+        type: 'Semi-Detached',
+        newHome: true,
+        recentlyAdded: false,
+    },
+
+    {   
+        images: 'https://media.rightmove.co.uk/120k/119425/126063677/119425_SEF220610_IMG_00_0000.jpeg',
+        address: 'Harris Drive, Liverpool, L20',
+        description: 'A bright modern two bedroom apartment situated on the first floor of this popular development on Highfield Street which runs parallel with Pall Mall close to its junction with Leeds Street. The property is close to Liverpool John Moores University and immediate vacant possession is available upon completion.',
+        price: 160000,
+        bedrooms: 3,
+        bathrooms: 1,
+        type: 'Semi-Detached',
+        newHome: true,
+        recentlyAdded: true,
+
+    },
+
+    {
+        images: 'https://media.rightmove.co.uk/204k/203528/127002389/203528_1433138-1_IMG_00_0000.jpeg',
+        address: 'Carnarvon Road, Liverpool, L9',
+        description: 'A bright modern two bedroom apartment situated on the first floor of this popular development on Highfield Street which runs parallel with Pall Mall close to its junction with Leeds Street. The property is close to Liverpool John Moores University and immediate vacant possession is available upon completion.',
+        price: 89950,
+        bedrooms: 3,
+        bathrooms: 1,
+        type: 'Terraced',
+        newHome: false,
+        recentlyAdded: true,
+    },
+
+    {
+        images: 'https://media.rightmove.co.uk/73k/72919/129159044/72919_18276_IMG_00_0000.jpeg',
+        address: '41 Butterfield Street, Liverpool',
+        description: 'A bright modern two bedroom apartment situated on the first floor of this popular development on Highfield Street which runs parallel with Pall Mall close to its junction with Leeds Street. The property is close to Liverpool John Moores University and immediate vacant possession is available upon completion.',
+        price: 60000,
+        bedrooms: 2,
+        bathrooms: 1,
+        type: 'Terraced',
+        newHome: false,
+        recentlyAdded: true,
+    },
+
+    {
+        images: 'https://media.rightmove.co.uk/75k/74061/127067960/74061_HSS248313_IMG_02_0000.jpeg',
+        address: 'Finch Lea Drive, Liverpool, L14',
+        description: 'A bright modern two bedroom apartment situated on the first floor of this popular development on Highfield Street which runs parallel with Pall Mall close to its junction with Leeds Street. The property is close to Liverpool John Moores University and immediate vacant possession is available upon completion.',
+        price: 120000,
+        bedrooms: 3,
+        bathrooms: 1,
+        type: 'Semi-Detached',
+        newHome: true,
+        recentlyAdded: false,
+    },
+
+    {   
+        images: 'https://media.rightmove.co.uk/120k/119425/126063677/119425_SEF220610_IMG_00_0000.jpeg',
+        address: 'Harris Drive, Liverpool, L20',
+        description: 'A bright modern two bedroom apartment situated on the first floor of this popular development on Highfield Street which runs parallel with Pall Mall close to its junction with Leeds Street. The property is close to Liverpool John Moores University and immediate vacant possession is available upon completion.',
+        price: 160000,
+        bedrooms: 3,
+        bathrooms: 1,
+        type: 'Semi-Detached',
+        newHome: false,
+        recentlyAdded: true,
+    },
+
+    {
+        images: 'https://media.rightmove.co.uk/204k/203528/127002389/203528_1433138-1_IMG_00_0000.jpeg',
+        address: 'Carnarvon Road, Liverpool, L9',
+        description: 'A bright modern two bedroom apartment situated on the first floor of this popular development on Highfield Street which runs parallel with Pall Mall close to its junction with Leeds Street. The property is close to Liverpool John Moores University and immediate vacant possession is available upon completion.',
+        price: 89_950,
+        bedrooms: 3,
+        bathrooms: 1,
+        type: 'Terraced',
+        newHome: false,
+        recentlyAdded: true,
+    },
+
+    {
+        images: 'https://media.rightmove.co.uk/73k/72919/129159044/72919_18276_IMG_00_0000.jpeg',
+        address: '41 Butterfield Street, Liverpool',
+        description: 'A bright modern two bedroom apartment situated on the first floor of this popular development on Highfield Street which runs parallel with Pall Mall close to its junction with Leeds Street. The property is close to Liverpool John Moores University and immediate vacant possession is available upon completion.',
+        price: 60000,
+        bedrooms: 2,
+        bathrooms: 1,
+        type: 'Terraced',
+        newHome: false,
+        recentlyAdded: true,
+    },
+]
+
+const contentContainer = document.querySelector(".content-container")
+
+function ready() {
+
+    function renderProperties(property) {
+        let html = ''
+        
+        property.forEach((house) => {
+
+        html = html + `
+        <div class='house-card'>
+
+            <div class='image-container'>
+                <div class="bookmark-icon">
+                    <i class="bi-suit-heart-fill bookmark"></i>
+                </div>
+                <img src=${house.images} alt="" />
+            </div>
+
+            <div class='text-container'>
+                <div class='house-details'>
+                    <h3>${house.address}</h3>
+    
+                    <div class="icon-container">
+                        <p class="bedrooms"><img class="icon" src="icons/icons8-bedroom-96.png" alt="">${house.bedrooms}</p>
+                        <p class="bathrooms"><img class="icon" src="icons/icons8-bath-100.png" alt="">${house.bathrooms}</p>
+                        <p class="type"><i class="bi bi-house"></i>${house.type}</p>
+                    </div>
+                </div>
+
+                <div class="description">
+                    <h2>Description</h2>
+                    <p>${house.description}</p>
+                </div>
+
+                <div class="button-container">
+                    <h2 class="price">£${house.price}</h2>                    
+                    <button class="view-property">
+                        <p>View Property</p>
+                    </button>
+                </div>
+            </div>
+        </div>
+        `
+        })
+
+        contentContainer.insertAdjacentHTML('beforeend', html)
+    }
+
+renderProperties(propertyListings)
+
+// Filter Lists for Quick Searches
+
+const newHomes = propertyListings.filter((item) => {
+    return item.newHome === false;
+})
+
+const recentlyAdded = propertyListings.filter((item) => {
+    return item.recentlyAdded === true;
+})
+
+const twoBedHouses = propertyListings.filter((item) => {
+    return item.bedrooms === 2;
+})
+
+const threeBedHouses = propertyListings.filter((item) => {
+    return item.bedrooms === 3;
+})
+
+function toggleOptions(list) {
+    contentContainer.innerHTML = ''
+    renderProperties(list)
+    addBookMark()
+}
+
+const toggleLists = [propertyListings, newHomes, recentlyAdded, twoBedHouses, threeBedHouses]
+const toggleMenuSearchButtons = document.querySelectorAll('ul.toggle-searches li')
+
+for (let i = 0; i < toggleMenuSearchButtons.length; i++) {
+    toggleMenuSearchButtons[i].addEventListener('click', function() {
+        toggleOptions(toggleLists[i])
+    })
+}
+
+const belowMarketButton = document.querySelector('.below-market-value')
+
+belowMarketButton.addEventListener('click', function() {
+    total = 0;
+
+    for (let i = 0; i < houseCards.length; i++ ) {
+        total += parseFloat(houseCards[i].lastElementChild.lastElementChild.
+        querySelector('h2').innerText.replace(/£/g, ''));
+    }
+
+    let totalProperties = houseCards.length;
+    let averagePrice = (total/totalProperties);
+
+    for (let i = 0; i < houseCards.length; i++ ) {
+        let houses = houseCards[i].lastElementChild.lastElementChild.
+        querySelector('h2').innerText.replace(/£/g, '');
+
+        if (houses <= averagePrice) {
+            houseCards[i].classList.remove('hidden')
+        } else {
+            houseCards[i].classList.add('hidden')
+        }
+    }
+})
+
+}
+
+ready()
+
+// Price slider code
+
+const priceSlider = document.getElementById('slider')
+const sliderValue = document.querySelector('#slider-value')
+const houseCards = contentContainer.children;
+sliderValue.innerHTML = priceSlider.value;
+
+priceSlider.oninput = function() {
+    let value = (this.value - this.min)/(this.max - this.min)*100;
+    priceSlider.style.background = 'linear-gradient(to right, #1a1a1a 0%' + value + '%, #1a1a1a' + value + '%, #f1f1f1 50%, #f1f1f1 100%)';
+
+    sliderValue.innerHTML = Math.round((this.value/1000))*1000;
+    const sliderPrice = parseInt(sliderValue.innerHTML)
+
+    for (let i = 0; i < houseCards.length; i++) {
+        const housePrice = houseCards[i].lastElementChild.lastElementChild.querySelector('h2')
+        .innerText.replace(/£/g, '');
+        const price = parseInt(housePrice)
+
+        if (price > sliderPrice) {
+            houseCards[i].classList.add('hidden')
+        } else {
+            houseCards[i].classList.remove('hidden')
+        }
+    }
+}
+
+// Bookmark Code
+
+const bookmark = document.getElementsByClassName('bookmark-icon');
+const savedPropertiesContainer = document.querySelector('.saved-property-list');
+const bookmarkEmptyMessage = document.querySelector('.panel-top p');
+const bookmarkCount = document.getElementById('bookmark-count');
+
+function checkBookMarkIsEmpty() {
+    if (localStorage.getItem('saved-properties') !== '0') {
+        bookmarkCount.style.backgroundColor = '#f93c3c';
+        bookmarkEmptyMessage.classList.add('hidden');
+    } else {
+        bookmarkCount.style.backgroundColor = '#b3b3b3'
+        bookmarkEmptyMessage.classList.remove('hidden');
+    }
+}
+
+checkBookMarkIsEmpty()
+
+function addBookMark() {
+
+for (let i = 0; i < bookmark.length; i++ ) {
+    bookmark[i].addEventListener('click', () => {
+
+        const image = houseCards[i].firstElementChild.querySelector('img').src;
+        const address = houseCards[i].querySelector('h3').innerText;
+        const price = parseInt(houseCards[i].querySelector('h2.price').innerText.replace(/£/g, ''));
+        const bedrooms = houseCards[i].querySelector('p.bedrooms').innerText;
+        const bathrooms = houseCards[i].querySelector('p.bathrooms').innerText;
+        const houseType = houseCards[i].querySelector('p.type').innerText;
+
+        savedPropertiesContainer.innerHTML += `
+
+            <div class="bookmark-card">
+                <div class="image">
+                    <div class="remove-bookmark-button">
+                        <i class="bi bi-x bookmark"></i>
+                    </div>
+                    <img src="${image}" alt="">
+                </div>
+
+                <div class="details">
+                    <h3 class="address">${address}</h3>
+                    <div class="icon-container">
+                        <p class="bedrooms"><img class="icon" src="icons/icons8-bedroom-96.png" alt="">${bedrooms}</p>
+                        <p class="bathrooms"><img class="icon" src="icons/icons8-bath-100.png" alt="">${bathrooms}</p>
+                        <p class="type"><i class="bi bi-house"></i>${houseType}</p>
+                    </div>
+
+                    <div class="button-container">
+                        <h2 class="price">£${price}</h2>                    
+                        <button class="view-property">
+                            <p>View Property</p>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        `;
+
+        checkBookMarkIsEmpty()
+        savePropertyListings()
+    });
+}}
+
+function savePropertyListings() {
+    localStorage.setItem('property-listings', savedPropertiesContainer.innerHTML);
+    let bookmarksCount = savedPropertiesContainer.children.length;
+    localStorage.setItem('saved-properties', bookmarksCount);
+    let count = localStorage.getItem('saved-properties');
+    bookmarkCount.innerText = count;
+    checkBookMarkIsEmpty()
+    removeBookmark()
+}
+
+function renderSavedProperties() {
+    let bookmarks = localStorage.getItem('property-listings');
+    let count = localStorage.getItem('saved-properties');
+
+    if (count == null && bookmarks == null) {
+        savedPropertiesContainer.innerHTML = ''
+        bookmarkCount.innerText = '0';
+    } else {
+        savedPropertiesContainer.insertAdjacentHTML('afterbegin', bookmarks);
+        bookmarkCount.innerText = count;
+    }
+
+    checkBookMarkIsEmpty()
+    removeBookmark()
+}
+
+function removeBookmark() {
+    const bookmarkedProperties = savedPropertiesContainer.children;
+    for (let i = 0; i < bookmarkedProperties.length; i++ ) {
+        bookmarkedProperties[i].querySelector('.remove-bookmark-button').addEventListener('click', deleteBookmark);
+    }}
+
+    function deleteBookmark(event) {
+        let buttonClicked = event.target;
+        buttonClicked.parentElement.parentElement.parentElement.remove()
+        savePropertyListings()
+        checkBookMarkIsEmpty()
+    }
+
+
+renderSavedProperties()
+addBookMark()
+
+const dimBackground = document.querySelector('.dim-background')
+const bookmarksPanel = document.querySelector('.bookmarks-panel');
+const showBookmarksPanelButton = document.querySelector('li.show-bookmarks');
+const hideBookmarksPanelButton = document.querySelector('i.bi.bi-x-circle')
+
+function ZIndex(value) {
+    dimBackground.style.setZIndex = value;
+}
+
+showBookmarksPanelButton.addEventListener('click', () => {
+    bookmarksPanel.classList.add('visible');
+    dimBackground.classList.add('visible');
+});
+
+hideBookmarksPanelButton.addEventListener('click', () => {
+    bookmarksPanel.classList.remove('visible');
+    dimBackground.classList.remove('visible');
+});
+
+// Toggle View Code
+
+const toggleView = document.querySelector('.grid-view')
+const text = toggleView.querySelector('span')
+const toggleIcon = toggleView.firstChild;
+
+function toggleCardView() {
+    for (let i = 0; i < houseCards.length; i++) {
+        if (houseCards[i].classList.contains('list')) {
+            houseCards[i].classList.remove('list')
+        } else {
+            houseCards[i].classList.add('list')
+        }
+    }
+
+    if (toggleIcon.classList.contains('bi-grid')) {
+        toggleIcon.classList.remove('bi-grid')
+        toggleIcon.classList.add('bi-list-ul')
+        text.innerText = 'List View'
+    } else {
+        toggleIcon.classList.remove('bi-list-ul')
+        toggleIcon.classList.add('bi-grid')
+        text.innerText = 'Grid View'
+    }
+}
+
+toggleView.addEventListener('click', toggleCardView);
